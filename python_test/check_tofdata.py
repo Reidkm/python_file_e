2b3f8f07a16a93571d945a6a894ed9c88b2f4d20
Reidkm <wy280894218@163.com>
2020-03-27 10:26:04 +0800

    update check_tofdata

--- python_test/check_tofdata.py
+++ python_test/check_tofdata.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 import cv2
+import glob
 
 
 
@@ -17,12 +18,13 @@ if __name__ == '__main__':
                            [892,259],
                            [1011,265],
                            [1277,381]]) #创建二维数组line_point
-    for tof_data_path in glob.glob(r'C:\Users\x\Desktop\kk\*.jpg'):
+    for tof_data_path in glob.glob(r'/home/reid/python_file_e/20190920_tof/tof_data/*.png'):
             src_image = cv2.imread(tof_data_path, cv2.IMREAD_ANYCOLOR)
-            cv2.line(src_image,line_point[0],line_point[1],(0,0,255),2)
-            cv2.line(src_image,line_point[2],line_point[3],(0,0,255),2)
-            cv2.line(src_image,line_point[4],line_point[5],(0,0,255),2)
-            cv2.line(src_image,line_point[6],line_point[7],(0,0,255),2)
+            #print(type(line_point[0]))
+            cv2.line(src_image,tuple(line_point[0]),tuple(line_point[1]),(0,0,255),2)
+            #cv2.line(src_image,line_point[2],line_point[3],(0,0,255),2)
+            #cv2.line(src_image,line_point[4],line_point[5],(0,0,255),2)
+            #cv2.line(src_image,line_point[6],line_point[7],(0,0,255),2)
             cv2.imshow('src_image', src_image)
             cv2.waitKey()
     
